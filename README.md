ccc8137d6a084550bcf54d192ac460667f749a28
Mehmet Gökçe <mg@mehmetgoekce.com>
2020-01-02 21:27:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# mehmetgoekce.com
+
+[![Build Status](https://travis-ci.com/MehmetGoekce/mehmetgoekce.com.svg?branch=master)](https://travis-ci.com/MehmetGoekce/mehmetgoekce.com)
+
+*Personal Website of mehmetgoekce.com*
+
+It is build on:
+
+- [mezzio](https://github.com/mezzio/mezzio) 
+- [laminas-stratigility](https://github.com/laminas/laminas-stratigility) 
+- [league/plates](http://platesphp.com) for templating.
+
+
---
+++ src/App/Handler/HomePageHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
+use Mezzio\Plates\PlatesRenderer;
+use Mezzio\Router;
+use Mezzio\Template\TemplateRendererInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class HomePageHandler implements RequestHandlerInterface
+{
+    /** @var string */
+    private $containerName;
+
+    /** @var Router\RouterInterface */
+    private $router;
+
+    /** @var null|TemplateRendererInterface */
+    private $template;
+
+    public function __construct(
+        string $containerName,
+        Router\RouterInterface $router,
+        ?TemplateRendererInterface $template = null
+    )
+    {
+        $this->containerName = $containerName;
+        $this->router = $router;
+        $this->template = $template;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->template;
+
+        $data = [];
+
+        $this->containerName;
+
+        $this->router;
+        $this->template;
+
+
+        return new HtmlResponse($this->template->render('app::home-page', $data));
+    }
+}
